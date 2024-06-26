95dccdfebed811f3b234defc89ae804d6d836e0f
RodrigoAraujoc <137010092+RodrigoAraujoc@users.noreply.github.com>
2024-06-26 19:30:37 +0100

    Delete M1 directory

--- M1/client/src/services/categoryService.js
+++
@@ -1,6 +0,0 @@
-import http from './httpService';
-import api from '../config.json';
-
-export function getCategories() {
-    return http.get(api.apiEndpoint + "/categories");
-}
--- M1/client/src/services/thirdParty.js
+++
@@ -1,8 +0,0 @@
-import http from './httpService';
-import api from '../config.json';
-
-export function addSubscriber(email){
-    return http.post(api.thirdParty, {
-        email
-    })
-}
